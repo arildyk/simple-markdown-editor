e3ae1c5b7bb56647d72ce8f437d29de249359d84
Arilol <65564205+Arilol@users.noreply.github.com>
2021-03-26 14:57:46 -0400

    Modified folder to have the MVC design pattern

---
+++ src/Controller/App.java
@@ -0,0 +1,16 @@
+package Controller;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Hey y'all!");
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
