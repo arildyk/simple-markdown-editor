7f940b8a37705b9716f759250f25e20934a46380
arildyk <65564205+arildyk@users.noreply.github.com>
2021-05-07 00:48:31 -0400

    Updated Main.fxml

---
+++ src/App.java
@@ -0,0 +1,57 @@
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class App extends Application {
+    
+    private Parent main;
+    private double xOffset, yOffset;
+    private Scene scene;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        try {
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(App.class.getResource("Views/Main.fxml"));
+
+            main = (Parent) loader.load();
+            scene = new Scene(main);
+
+            primaryStage.initStyle(StageStyle.TRANSPARENT);
+            scene.setFill(Color.TRANSPARENT);
+            scene.getStylesheets().add("Views/text-area-background.css");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+            main.setOnMousePressed(new EventHandler<MouseEvent>(){
+                @Override
+                public void handle(MouseEvent event) {
+                    xOffset = event.getSceneX();
+                    yOffset = event.getSceneY();
+                }
+            });
+
+            main.setOnMouseDragged(new EventHandler<MouseEvent>(){
+                @Override
+                public void handle(MouseEvent event) {
+                    primaryStage.setX(event.getScreenX() - xOffset);
+                    primaryStage.setY(event.getScreenY() - yOffset);
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/Controllers/AppController.java
@@ -0,0 +1,23 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.web.WebView;
+
+public class AppController implements Initializable {
+    
+    @FXML
+    private TextArea input;
+
+    @FXML
+    private WebView output;
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
