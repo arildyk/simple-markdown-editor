1530fa2878828a05c49e40ff21b86d3d3b0b7f14
arildyk <65564205+arildyk@users.noreply.github.com>
2021-05-12 04:28:07 -0400

    Added features and fixed bugs
    - Added support for tables, task list items, images attributes, underlined text, and strikethrough text
    - Links now opens a new tab on the user's default browser
    - Fixed a bug where the input was not rendered when the user did a copy and paste
    -  Fixed a bug where pressing enter is not rendered

--- src/Controllers/AppController.java
+++ src/Controllers/AppController.java
@@ -1,19 +1,37 @@
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import org.commonmark.Extension;
+import org.commonmark.ext.gfm.strikethrough.StrikethroughExtension;
+import org.commonmark.ext.gfm.tables.TablesExtension;
+import org.commonmark.ext.image.attributes.ImageAttributesExtension;
+import org.commonmark.ext.ins.InsExtension;
+import org.commonmark.ext.task.list.items.TaskListItemsExtension;
 import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.AttributeProvider;
 import org.commonmark.renderer.html.AttributeProviderContext;
 import org.commonmark.renderer.html.AttributeProviderFactory;
 import org.commonmark.renderer.html.HtmlRenderer;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.events.Event;
+import org.w3c.dom.events.EventListener;
+import org.w3c.dom.events.EventTarget;
+import org.w3c.dom.html.HTMLAnchorElement;
 
 import Models.FontAttributeProvider;
 import javafx.application.Platform;
+import javafx.concurrent.Worker.State;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.WebView;
@@ -27,10 +45,9 @@ public class AppController implements Initializable {
     private WebView output;
 
     @FXML
-    private Button exitButton;
-
-    @FXML
     private AnchorPane mainScene;
+
+    private List<Extension> extensions;
 
     public void exit(ActionEvent actionEvent) {
         Platform.exit();
@@ -38,16 +55,9 @@ public class AppController implements Initializable {
 
     public void parse() {
 
-        // Change font
-        // Add table support
-        // Add task list support
-        // Get link to open in another browser
-        // Fix enter bug
-        // Add 'onPaste' method
+        Parser parser = Parser.builder().extensions(extensions).build();
 
-        Parser parser = Parser.builder().build();
-
-        HtmlRenderer renderer = HtmlRenderer.builder().attributeProviderFactory(new AttributeProviderFactory() {
+        HtmlRenderer renderer = HtmlRenderer.builder().extensions(extensions).attributeProviderFactory(new AttributeProviderFactory() {
 
             @Override
             public AttributeProvider create(AttributeProviderContext context) {
@@ -59,12 +69,70 @@ public class AppController implements Initializable {
 
         Node doc = parser.parse(input.getText());
         String outgoing = renderer.render(doc);
-        output.getEngine().loadContent(outgoing);
+        String outString = outgoing.replace("\n", "<br>");
+        String processedString = outString.replace("><br><", ">\n<");
+
+        System.out.println(processedString);
+
+        output.getEngine().loadContent(processedString);
     }
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         // TODO Auto-generated method stub
+
+        extensions = Arrays.asList(TablesExtension.create(), StrikethroughExtension.create(), InsExtension.create(), ImageAttributesExtension.create(), TaskListItemsExtension.create());
+
+        output.getEngine().getLoadWorker().stateProperty().addListener((observableValue, oldValuState, nState) -> {
+
+            if (nState == State.SUCCEEDED) {
+
+                Document document = output.getEngine().getDocument();
+
+                NodeList nodeList = document.getElementsByTagName("a");
+
+                for (int i = 0; i < nodeList.getLength(); i++) {
+
+                    org.w3c.dom.Node node= nodeList.item(i);
+
+                    EventTarget eventTarget = (EventTarget) node;
+
+                    eventTarget.addEventListener("click", new EventListener() {
+                        
+                        @Override
+                        public void handleEvent(Event evt) {
+                            
+                            EventTarget target = evt.getCurrentTarget();
+                            HTMLAnchorElement anchorElement = (HTMLAnchorElement) target;
+                            String href = anchorElement.getHref();
+
+                            //Opening the URL outside the WebView
+                            evt.preventDefault();
+
+                            if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+                                try {
+                                    Desktop.getDesktop().browse(new URI(href));
+                                } catch (IOException e) {
+                                    // TODO Auto-generated catch block
+                                    e.printStackTrace();
+                                } catch (URISyntaxException e) {
+                                    // TODO Auto-generated catch block
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+
+                    }, false);
+                }
+                
+            }
+        });
+
+        input.textProperty().addListener((observableValue, oldValue, newValue) -> {
+            parse();
+        });
+        
+
     }
 
 }
--- src/Models/FontAttributeProvider.java
+++ src/Models/FontAttributeProvider.java
@@ -2,6 +2,7 @@ package Models;
 
 import java.util.Map;
 
+import org.commonmark.ext.gfm.tables.TableBlock;
 import org.commonmark.node.Heading;
 import org.commonmark.node.Node;
 import org.commonmark.node.Paragraph;
@@ -16,6 +17,10 @@ public class FontAttributeProvider implements AttributeProvider {
         
         if (node instanceof Heading) {
             attributes.put("style", "@font-face { font-family: EB Garamond; src: url(Fonts/EBGaramond-Regular.ttf); } @font-face { font-family: EB Garamond; src: url(Fonts/EBGaramond-Bold.ttf); font-weight: bold; }  @font-face { font-family: EB Garamond; src: url(Fonts/EBGaramond-Italic.ttf); font-style: italic; } @font-face { font-family: EB Garamond; src: url(Fonts/EBGaramond-ExtraBold.ttf); font-weight: bolder; } @font-face { font-family: EB Garamond; src: url(Fonts/EBGaramond-ExtraBoldItalic.ttf); font-weight: bolder; font-style: italic; } @font-face { font-family: EB Garamond; src: url(Fonts/EBGaramond-BoldItalic.ttf); font-weight: bold; font-style: italic; } font-family: EB Garamond");
+        }
+
+        if (node instanceof TableBlock) {
+            attributes.put("border", "1");
         }
     }
 }
